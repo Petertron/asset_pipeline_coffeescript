681e7974234581a25937e021fe4912be46a4adad
Peter Skirenko <petertron@mailservice.ms>
2017-03-04 20:35:55 +0000

    Version 0.2.1.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Asset Pipeline CoffeeScript Compiler version 0.2
+# Asset Pipeline CoffeeScript Compiler version 0.2.1
 
-This extension for Symphony CMS goes together with the [Asset Pipeline](https://github.com/Petertron/asset_pipeline/) extension and provides a compiler for CoffeeScript version 1.12.2.
+This extension for Symphony CMS goes together with the [Asset Pipeline](https://github.com/petertron/asset_pipeline/) extension and provides a compiler for CoffeeScript version 1.12.4.
 
-For an explanation of how to use Asset Pipeline see <https://petertron.github.io/asset_pipeline/>.
+For an explanation of how to use Asset Pipeline see <http://git.io/sym-asset-pipeline/>.
 
 ## Requirements
 
+- Asset Pipeline 0.2+ must be installed.
 - Node.js must be installed.
 - PHP `exec` command must be enabled.
--- extension.driver.php
+++ extension.driver.php
@@ -1,29 +1,56 @@
 <?php
 
-class extension_asset_pipeline_coffeescript extends Extension
+class extension_Asset_pipeline_coffeescript extends Extension
 {
+    public $error;
+
+    public function getOutputType()
+    {
+        return 'js';
+    }
+
     public function getSubscribedDelegates()
     {
         return array(
             array(
                 'page' => '/extension/asset_pipeline/',
-                'delegate' => 'RegisterPlugins',
+                'delegate' => 'RegisterPreprocessors',
                 'callback' => 'register'
             )
         );
     }
 
-    function register($context)
+    public function register($context)
     {
-        $context['plugins']['coffee'] = array('output_type' => 'js', 'driver' => $this);
+        $context['preprocessors']['coffee'] = $this;
     }
 
-    public function compile($content)
+    public function convert($content, $import_dir)
     {
-        $result = shell_exec(
-            'node ' . escapeshellarg(__DIR__ . '/lib/compile.js') . ' ' . escapeshellarg($content)
+        $process = proc_open(
+            'node lib/compile.js '
+                . escapeshellarg($content) . ' '
+                . escapeshellarg($import_dir),
+            array(
+                0 => array('pipe', 'r'),
+                1 => array('pipe', 'w'),
+                2 => array('pipe', 'w')
+            ),
+            $pipes, __DIR__
         );
-        $pos = strpos($result, ' ');
-        return array(substr($result, 0, $pos) => substr($result, $pos + 1));
+
+        if ($error = stream_get_contents($pipes[2])) {
+            $this->error = $error;
+        } else {
+            $content = stream_get_contents($pipes[1]);
+        }
+
+        foreach ($pipes as $pipe) {
+            fclose($pipe);
+        }
+        proc_close($process);
+
+        return $content;
     }
+
 }
